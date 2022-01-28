d3d49b635bc82eb00729337ac544f7d251ea718d
Dhafin21 <86392758+Dhafin21@users.noreply.github.com>
2022-01-28 21:32:31 +0700

    Update coba_coba.py

--- coba_coba.py
+++ coba_coba.py
@@ -1,17 +1,17 @@
 from browser import document, alert  
 import math
 
-# Deklarasi Variable
+
 data1 = document['data1']
 data2 = document['data2']
 button = document['btn']
 output = document['output']
 
-# Dictionary
-type1 = {'formula': lambda TB, BB: round(BB / ((TB/100) ** 2), 1),
-         'input1': 'Tinggi Badan (cm)', 'input2': 'Berat Badan (kg)'}
 
-# Fungsi untuk mengubah string dari input ke int atau float
+hitungbmi = {'rumus': lambda TB, BB: round(BB / ((TB/100) ** 2), 1),
+         'data1': 'Tinggi Badan (cm)', 'data2': 'Berat Badan (kg)'}
+
+
 def getNum(x):
     temp = x
     try:
@@ -27,24 +27,21 @@ def getNum(x):
         else:
             return temp
 
-# Fungsi untuk memanggil formula pada dictionary
-def formula(num1, num2):
-    for key in type1.keys():
-        return type1['formula'](num1, num2)
+def rumus(num1, num2):
+    for key in hitungbmi.keys():
+        return hitungbmi['rumus'](num1, num2)
 
-# Fungsi Main
 def main(ev):
     num1 = getNum(data1.value)
     num2 = getNum(data2.value)
-    result = formula(num1, num2)
+    result = rumus(num1, num2)
     output.textContent = str(result)
 
-# Fungsi keyEnter
 def keyEnter(ev):
     traceKey = f"{ev.code}"
     if traceKey == 'Enter':
         main(0)
 
-button.bind('click', main)  # Memanggil 'Fungsi Main' ketika button di-click
+button.bind('click', main)  
 
 
